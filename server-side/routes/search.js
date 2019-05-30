ec2b37c680c04a95c2d8cd7e4d0e762b2b5077d9
Unknown <shannon.herondale@gmail.com>
2019-05-30 20:17:22 +1000

    search with optional parameters

--- server-side/routes/search.js
+++ server-side/routes/search.js
@@ -2,12 +2,19 @@ var express = require('express');
 // const mysql = require('mysql');
 var router = express.Router();
 
-router.get("/search/offence=?:offence", function (req, res, next) { //offence&:area&:age&:gender&:year:&month
+router.get("/search/offence=:offence?(&area=:area)?(&age=:age)?(&gender=:gender)?(&year=:year)?(&month=:month)?", function (req, res, next) {
+    let offence = decodeURIComponent(req.params.offence);
+    let area = decodeURIComponent(req.params.area);
+    let age = decodeURIComponent(req.params.age);
+    let gender = decodeURIComponent(req.params.gender);
+    let year = decodeURIComponent(req.params.year);
+    let month = decodeURIComponent(req.params.month);
+    
     let query = req.db.from("offences").innerJoin('areas', 'offences.area', 'areas.area')
 
-    
-    query.select(req.db.raw('offences.area, areas.lat, areas.lng, count(??)', [decodeURIComponent(req.params.offence)]))
-    query.whereRaw("?? <> 0", [decodeURIComponent(req.params.offence)])
+
+    query.select(req.db.raw('offences.area, areas.lat, areas.lng, sum(??)', [offence]))
+    query.whereRaw("?? <> 0", [offence])
     query.groupBy(['offences.area', 'areas.lat', 'areas.lng'])
     query.then((rows) => {
             res.json({
@@ -24,12 +31,19 @@ router.get("/search/offence=?:offence", function (req, res, next) { //offence&:a
         })
 });
 
-router.get("/search/test/offence=?:offence", function (req, res, next) {//offence&:area&:age&:gender&:year:&month
+router.get("/search/test/offence=:offence?(&area=:area)?(&age=:age)?(&gender=:gender)?(&year=:year)?(&month=:month)?", function (req, res, next) {
+    let offence = decodeURIComponent(req.params.offence);
+    let area = decodeURIComponent(req.params.area);
+    let age = decodeURIComponent(req.params.age);
+    let gender = decodeURIComponent(req.params.gender);
+    let year = decodeURIComponent(req.params.year);
+    let month = decodeURIComponent(req.params.month);
+    
     let query = req.db.from("offences").innerJoin('areas', 'offences.area', 'areas.area')
 
 
-    query.select(req.db.raw('offences.area, areas.lat, areas.lng, sum(??)', [decodeURIComponent(req.params.offence)]))
-    query.whereRaw("?? <> 0", [decodeURIComponent(req.params.offence)])
+    query.select(req.db.raw('offences.area, areas.lat, areas.lng, sum(??)', [offence]))
+    query.whereRaw("?? <> 0", [offence])
     query.groupBy(['offences.area', 'areas.lat', 'areas.lng'])
     query.then((rows) => {
             res.json({
